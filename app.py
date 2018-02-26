afea98a18fcf1c0c6b0773d7bb9bbe19c403b794
Bibi <serge_cachia@hotmail.com>
2018-02-26 16:51:38 +0100

    Added PostgreSQL environment and psycopg2.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ Created on Wed Feb 22 12:16:31 2018
 @author: bibi
 """
 
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -18,14 +20,10 @@ from resources.store import Store, StoreList
 # a resource represents something an api represents ...
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" # at the root folder of our project
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db") # at the root folder of our project
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # turns off the flask sqlalchemy tracker but not the sqlalchemy tracker ... subtle
 app.secret_key = 'jose'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # create new endpoint /auth
 
